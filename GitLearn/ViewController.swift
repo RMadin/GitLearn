ec11d826bfa099c4d80bd31cd4d6c49355a34132
Madina Rahimova <rmadina1985@gmail.com>
2026-03-05 13:49:36 +0500

    Initial Commit

---
+++ GitLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitLearn
+//
+//  Created by Madina on 05/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
