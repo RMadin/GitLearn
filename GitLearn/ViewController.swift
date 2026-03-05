0ae5b71eade3b07cbaef539e663b151735463474
Madina Rahimova <rmadina1985@gmail.com>
2026-03-05 14:07:07 +0500

    some changes

--- GitLearn/ViewController.swift
+++ GitLearn/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
+        
+        let someLabel = UILabel()
+        
+        someLabel.text = "Hello"
+        someLabel.textColor = .green
+        someLabel.font = UIFont(name: "PostScriptName", size: 20)
+        someLabel.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        
+        
+        view.addSubview(someLabel)
     }
 
 
